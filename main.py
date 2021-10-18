b87bb37da78a9f4d621491929540b66acbd4568b
undrcrxwn <69521267+undrcrxwn@users.noreply.github.com>
2021-10-18 22:28:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 print('Application is running')
-print('<resolved merge-conflict>')
+print('<resolved merge-conflict> + some changes from GitHub')
 
 for i in range(5):
 	print('Iteration N', i + 1)
